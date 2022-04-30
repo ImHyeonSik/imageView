f3b392178ce11e46d1e5547d89cba2a21dcfbaf0
hyeonsik <dgt4862@gmail.com>
2022-04-30 17:12:18 +0900

    imageView 확대 및 스위치 사용

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
 
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "food_on.png")
+        imgOff = UIImage(named: "food_off.png")
+        
+//        imgView.image = imgOn
+        imgView.image = imgOn
     }
 
 
+    @IBAction func btnResizeImage(_ sender: Any) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+            
+        }
+        else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+            
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+        
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        }
+        else {
+            imgView.image = imgOff
+            
+        }
+        
+    }
 }
 
